cba8f4eee4bd953bf571198382ad1f6f2aa61c60
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-12 23:52:56 +0100

    Add getAudioFeatures.

--- src/Track/index.js
+++ src/Track/index.js
@@ -4,4 +4,8 @@ export class TrackAPI extends CoreAPI {
   getAudioAnalysis(trackId) {
     return this.request(this.url(`v1/audio-analysis/${trackId}`));
   }
+
+  getAudioFeatures(trackId) {
+    return this.request(this.url(`v1/audio-features/${trackId}`));
+  }
 }

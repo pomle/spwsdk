1807aa404a5dd16f044e92b0e12d0491fab958c1
Pontus Persson <pontus@warpinmedia.com>
2018-01-16 19:13:29 +0100

    Add track handling to PlaylistAPI.

--- src/Playlist/__tests__/PlaylistAPI.test.js
+++ src/Playlist/__tests__/PlaylistAPI.test.js
@@ -3,6 +3,43 @@ import {PlaylistAPI} from '../';
 describe('PlaylistAPI', () => {
     const FAKE_RESULT = Symbol();
 
+    describe('#addTracks()', () => {
+        it('creates a payload without position if omitted', () => {
+            const api = new PlaylistAPI();
+            api.request = jest.fn().mockReturnValue(FAKE_RESULT);
+            expect(api.addTracks('pomle', '29851vj901jf2ck', ['k1fj24fj1241', '1209c1-2k', '192ki-01v2rv'])).toBe(FAKE_RESULT);
+            expect(api.request.mock.calls.length).toBe(1);
+            expect(api.request).toBeCalledWith(
+                'https://api.spotify.com/v1/users/pomle/playlists/29851vj901jf2ck/tracks',
+                {"uris": ["spotify:track:k1fj24fj1241", "spotify:track:1209c1-2k", "spotify:track:192ki-01v2rv"]},
+                'POST');
+        });
+
+        it('creates a payload with position if given', () => {
+            const api = new PlaylistAPI();
+            api.request = jest.fn().mockReturnValue(FAKE_RESULT);
+            expect(api.addTracks('pomle', '29851vj901jf2ck', ['k1fj24fj1241', '1209c1-2k', '192ki-01v2rv'], 10)).toBe(FAKE_RESULT);
+            expect(api.request.mock.calls.length).toBe(1);
+            expect(api.request).toBeCalledWith(
+                'https://api.spotify.com/v1/users/pomle/playlists/29851vj901jf2ck/tracks',
+                {"position": 10, "uris": ["spotify:track:k1fj24fj1241", "spotify:track:1209c1-2k", "spotify:track:192ki-01v2rv"]},
+                'POST');
+        });
+    });
+
+    describe('#removeTrack()', () => {
+        it('creates a compatible payload', () => {
+            const api = new PlaylistAPI();
+            api.request = jest.fn().mockReturnValue(FAKE_RESULT);
+            expect(api.removeTrack('pomle', '29851vj901jf2ck', '12r1c02', 'k1fj24fj1241', 2)).toBe(FAKE_RESULT);
+            expect(api.request.mock.calls.length).toBe(1);
+            expect(api.request).toBeCalledWith(
+                'https://api.spotify.com/v1/users/pomle/playlists/29851vj901jf2ck/tracks',
+                {"snapshot_id": "12r1c02", "tracks": [{"positions": [2], "uri": "spotify:track:k1fj24fj1241"}]},
+                'DELETE');
+        });
+    });
+
     describe('#getPlaylists()', () => {
         it('queries logged in users playlists if userId omitted', () => {
             const api = new PlaylistAPI();
--- src/Playlist/index.js
+++ src/Playlist/index.js
@@ -1,6 +1,22 @@
-import { CoreAPI } from '../CoreAPI.js';
+import { CoreAPI, trackURI } from '../CoreAPI.js';
 
 export class PlaylistAPI extends CoreAPI {
+  addTracks(userId, playlistId, trackIds, position = null) {
+    const payload = {
+      uris: trackIds.map(trackURI),
+    };
+
+    if (position !== null) {
+      payload.position = position;
+    }
+
+    return this.request(
+      this.url(`v1/users/${userId}/playlists/${playlistId}/tracks`),
+      payload,
+      'POST',
+    );
+  }
+
   getPlaylists(userId = 'me') {
     return this.request(this.url(`v1/${userId}/playlists`));
   }
@@ -14,6 +30,24 @@ export class PlaylistAPI extends CoreAPI {
   getPlaylistTracks(userId, playlistId) {
     return this.request(
       this.url(`v1/users/${userId}/playlists/${playlistId}/tracks`)
+    );
+  }
+
+  removeTrack(userId, playlistId, snapshotId, trackId, position) {
+    const payload = {
+      snapshot_id: snapshotId,
+      tracks: [
+        {
+          uri: trackURI(trackId),
+          positions: [position],
+        },
+      ],
+    };
+
+    return this.request(
+      this.url(`v1/users/${userId}/playlists/${playlistId}/tracks`),
+      payload,
+      'DELETE',
     );
   }
 }

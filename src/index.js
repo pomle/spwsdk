491b107926b8d65caecdbc5f9ba04874d6ff7f45
Pontus Persson <pontus@warpinmedia.com>
2018-01-26 13:53:54 +0100

    Remove deprecated methods.

--- src/index.js
+++ src/index.js
@@ -35,38 +35,3 @@ const playerPromise = new Promise(resolve => {
 export function getSpotify() {
   return playerPromise;
 }
-
-export function createPlayer(token, options) {
-  return getSpotify()
-  .then(Spotify => {
-    const player = new Spotify.Player(Object.assign({
-      getOAuthToken: callback => callback(token),
-    }, options));
-
-    return player;
-  });
-}
-
-export function createAuthorizationURL(clientId, callbackURL) {
-  return (
-    'https://accounts.spotify.com/authorize?' +
-    [
-      ['client_id', clientId],
-      ['redirect_uri', callbackURL],
-      ['response_type', 'token'],
-      [
-        'scope',
-        [
-          'user-read-private',
-          'user-read-playback-state',
-          'user-modify-playback-state',
-          'playlist-read-private',
-          'playlist-read-collaborative',
-          'streaming',
-        ].join(' '),
-      ],
-    ]
-      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-      .join('&')
-  );
-}
